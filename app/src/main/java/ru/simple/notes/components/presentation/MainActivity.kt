fcd2eb511c5030f8c4cc532dbd2dcc45552552c6
Demetrius <the.great.demetrius@gmail.com>
2022-01-03 23:12:29 +0400

    Added splash screen

--- app/src/main/java/ru/simple/notes/components/presentation/MainActivity.kt
+++ app/src/main/java/ru/simple/notes/components/presentation/MainActivity.kt
@@ -3,10 +3,12 @@ package ru.simple.notes.components.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.graphics.toArgb
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import ru.simple.notes.ui.theme.DarkGray
 import ru.simple.notes.ui.theme.NoteTheme
@@ -14,8 +16,16 @@ import ru.simple.notes.ui.theme.NoteTheme
 @ExperimentalAnimationApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepVisibleCondition {
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             NoteTheme {
                 window.navigationBarColor = DarkGray.toArgb()
